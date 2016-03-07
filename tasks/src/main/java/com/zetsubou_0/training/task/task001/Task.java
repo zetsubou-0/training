415298afeab139af26bedbc6d4dc781dd8d08bfe
zetsubou_0 <12357>
2016-03-07 15:53:29 +0300

    init

---
+++ tasks/src/main/java/com/zetsubou_0/training/task/task001/Task.java
@@ -0,0 +1,7 @@
+package com.zetsubou_0.training.task.task001;
+
+/**
+ * Created by zetsubou_0 on 07.03.16.
+ */
+public class Task {
+}
---
+++ tasks/src/main/java/com/zetsubou_0/training/task/task002/Task.java
@@ -0,0 +1,7 @@
+package com.zetsubou_0.training.task.task002;
+
+/**
+ * Created by zetsubou_0 on 07.03.16.
+ */
+public class Task {
+}
