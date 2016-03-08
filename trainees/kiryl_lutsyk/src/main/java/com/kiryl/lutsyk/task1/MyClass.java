d55571b7a975d174ee9a2068ae5b1af836fc88ea
zetsubou_0 <12357>
2016-03-08 15:16:51 +0300

    example

---
+++ trainees/kiryl_lutsyk/src/main/java/com/kiryl/lutsyk/task1/MyClass.java
@@ -0,0 +1,10 @@
+package com.kiryl.lutsyk.task1;
+
+import com.zetsubou_0.training.task.task002.Task;
+
+public class MyClass implements Task {
+
+    public String condition(int number) {
+        return null;
+    }
+}
--- trainees/kiryl_lutsyk/src/test/java/com/zetsubou_0/training/test/trainees/task002/Test.java
+++ trainees/kiryl_lutsyk/src/test/java/com/zetsubou_0/training/test/trainees/task002/Test.java
@@ -1,5 +1,6 @@
 package com.zetsubou_0.training.test.trainees.task002;
 
+import com.kiryl.lutsyk.task1.MyClass;
 import com.zetsubou_0.training.test.task002.TaskTest;
 import org.junit.Before;
 
@@ -7,6 +8,6 @@ public class Test extends TaskTest {
 
     @Before
     public void setup() throws InstantiationException, IllegalAccessException {
-//        setupClass(MyClass.class);
+        setupClass(MyClass.class);
     }
 }
