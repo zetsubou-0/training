8f90456403c78845e559cf589ba6386e0327608b
zetsubou_0 <12357>
2016-03-08 14:10:18 +0300

    test 1 example

--- trainees/kiryl_lutsyk/src/test/java/com/zetsubou_0/training/test/trainees/task001/Test.java
+++ trainees/kiryl_lutsyk/src/test/java/com/zetsubou_0/training/test/trainees/task001/Test.java
@@ -1,12 +1,13 @@
 package com.zetsubou_0.training.test.trainees.task001;
 
 import com.zetsubou_0.training.test.task001.TaskTest;
+import mypackagename.subpackage.MyClassName;
 import org.junit.Before;
 
 public class Test extends TaskTest {
 
     @Before
     public void setup() throws InstantiationException, IllegalAccessException {
-//        setupClass(MyClass.class);
+        setupClass(MyClassName.class);
     }
 }
