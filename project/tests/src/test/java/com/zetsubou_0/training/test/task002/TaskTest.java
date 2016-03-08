c5d55b12c0c7c9d0c9adca8353766d5e3b8bab08
zetsubou_0 <12357>
2016-03-08 13:06:15 +0300

    init

---
+++ project/tests/src/test/java/com/zetsubou_0/training/test/task002/TaskTest.java
@@ -0,0 +1,26 @@
+package com.zetsubou_0.training.test.task002;
+
+import com.zetsubou_0.training.task.task002.Task;
+import com.zetsubou_0.training.test.util.BaseTest;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static com.zetsubou_0.training.test.util.TestConstant.Test002.*;
+
+public class TaskTest extends BaseTest<Task> {
+
+    @Test
+    public void shouldGetPositive() {
+        assertEquals(SHOULD_BE_POSITIVE, "positive", sut.condition(123));
+    }
+
+    @Test
+    public void shouldGetPositiveForZero() {
+        assertEquals(ZERO_VALUE, "zero", sut.condition(0));
+    }
+
+    @Test
+    public void shouldGetNegative() {
+        assertEquals(SHOULD_BE_NEGATIVE, "negative", sut.condition(-123));
+    }
+}
---
+++ project/trainees/sergei_timoshenko/src/test/java/com/zetsubou_0/training/test/trainees/task001/Test.java
@@ -0,0 +1,12 @@
+package com.zetsubou_0.training.test.trainees.task001;
+
+import com.zetsubou_0.training.test.task001.TaskTest;
+import org.junit.Before;
+
+public class Test extends TaskTest {
+
+    @Before
+    public void setup() throws InstantiationException, IllegalAccessException {
+//        setupClass(MyClass.class);
+    }
+}
