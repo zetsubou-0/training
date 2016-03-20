8687d520bf87641e14ddc26554ebadfc805b84e9
Torus <valiktorus@mail.ru>
2016-03-20 20:10:52 +0200

    Fibonacci Array

--- trainees/valentin_pshenichnik/src/test/java/com/zetsubou_0/training/test/trainees/task004/Test.java
+++ trainees/valentin_pshenichnik/src/test/java/com/zetsubou_0/training/test/trainees/task004/Test.java
@@ -1,11 +1,12 @@
 package com.zetsubou_0.training.test.trainees.task004;
 
 import com.zetsubou_0.training.test.task004.TaskTest;
+import tasks.Fibonacci;
 
 public class Test extends TaskTest {
 
     @Override
     public void init() throws IllegalAccessException, InstantiationException {
-//        setupClass(Fibonnaci.class);
+        setupClass(Fibonacci.class);
     }
 }
