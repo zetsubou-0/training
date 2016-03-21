75b74a9b6b161ff8967328ff6e22228de5703c1c
valiktorus <valiktorus@mail.ru>
2016-03-21 10:44:29 +0300

    Fibonacci Array

--- trainees/valentin_pshenichnik/src/main/java/tasks/Fibonacci.java
+++ trainees/valentin_pshenichnik/src/main/java/tasks/Fibonacci.java
@@ -8,12 +8,11 @@ import com.zetsubou_0.training.task.task004.Task;
 public class Fibonacci implements Task {
     public int[][] fibonacciArray(int columnsCount) {
         int fibonacci[][] = new int[columnsCount][];
-        for (int i = 0; i <columnsCount ; fibonacci[i] = new int[i+1], i++) {
-        }
         int summaFibonacci;
         int a = 1;
         int b = 1;
         for (int i = 0; i <columnsCount ; i++) {
+            fibonacci[i] = new int[i+1];
             for (int j = 0; j <i+1 ; j++) {
                 if ((i == 0 && j == 0) || (i == 1 && j == 0)){
                     fibonacci[i][j] = 1;
