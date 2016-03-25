d62d6983c824a017356b527367673d8350d9ff64
Kiryl Lutsyk <Kiryl_Lutsyk@epam.com>
2016-03-25 15:38:04 +0300

    qwe task

---
+++ trainees/vadim_martinenko/src/main/java/qwe/QweTask.java
@@ -0,0 +1,13 @@
+package qwe;
+
+import com.zetsubou_0.training.task.task001.Task;
+
+/**
+ * Created by Kiryl_Lutsyk on 3/25/2016.
+ */
+public class QweTask implements Task {
+
+    public boolean isPositive(final int number) {
+        return false;
+    }
+}
