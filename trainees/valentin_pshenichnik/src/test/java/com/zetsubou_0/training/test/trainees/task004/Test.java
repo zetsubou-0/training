11c8c376a9860b597dbe18e04e2813dd14e28baf
zetsubou-0 <zetsubou.zero.0@gmail.com>
2016-03-26 15:59:18 +0300

    Tests for 4 task was added

--- trainees/valentin_pshenichnik/src/test/java/com/zetsubou_0/training/test/trainees/task004/Test.java
+++ trainees/valentin_pshenichnik/src/test/java/com/zetsubou_0/training/test/trainees/task004/Test.java
@@ -1,12 +1,11 @@
 package com.zetsubou_0.training.test.trainees.task004;
 
 import com.zetsubou_0.training.test.task004.TaskTest;
-import tasks.Fibonacci;
 
 public class Test extends TaskTest {
 
     @Override
     public void init() throws IllegalAccessException, InstantiationException {
-        setupClass(Fibonacci.class);
+//        setupClass(MyClass.class);
     }
 }
