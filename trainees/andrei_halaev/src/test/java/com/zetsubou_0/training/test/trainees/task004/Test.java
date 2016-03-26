de967d5f04fbf3bd2c8fc6144f50f29220a263a5
zetsubou-0 <zetsubou.zero.0@gmail.com>
2016-03-26 16:04:33 +0300

    Tests for 4 task was added

---
+++ trainees/andrei_halaev/src/test/java/com/zetsubou_0/training/test/trainees/task004/Test.java
@@ -0,0 +1,11 @@
+package com.zetsubou_0.training.test.trainees.task004;
+
+import com.zetsubou_0.training.test.task004.TaskTest;
+
+public class Test extends TaskTest {
+
+    @Override
+    public void init() throws IllegalAccessException, InstantiationException {
+//        setupClass(MyClass.class);
+    }
+}
