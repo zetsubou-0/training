3cff997bfed472ea274fb2cf0ec43c64626efd3e
valiktorus <valiktorus@mail.ru>
2016-06-28 11:21:25 +0300

    Sorting arrays with ascending and descending methods

--- trainees/valentin_pshenichnik/src/main/java/tasks/task6/SortArray.java
+++ trainees/valentin_pshenichnik/src/main/java/tasks/task6/SortArray.java
@@ -2,16 +2,23 @@ package tasks.task6;
 
 import com.zetsubou_0.training.task.task006.Task;
 
+import java.util.Arrays;
+
 /**
  * Created by admin on 31.03.2016.
  */
 public class SortArray implements Task {
     public int[] ascendingSort(int[] array) {
-
-        return new int[0];
+        Arrays.parallelSort(array);
+        return array;
     }
 
     public int[] descendingSort(int[] array) {
+        int[] arrayNew = new int[array.length];
+        Arrays.parallelSort(array);
+        for (int i = 0; i <array.length ; i++) {
+            arrayNew[i] = array[array.length - i];
+        }
         return new int[0];
     }
 }
