ee00dc862b43776450bcb71d2aebb20fe1c7f713
Vaider <lurkto@mail.ru>
2016-07-19 18:52:14 +0300

    Task 1 #ispositive done.

--- trainees/vadim_martinenko/src/main/java/com/va/ma/task001/MyClass.java
+++ trainees/vadim_martinenko/src/main/java/com/va/ma/task001/MyClass.java
@@ -1,8 +1,18 @@
 package com.va.ma.task001;
 
 
+import com.zetsubou_0.training.task.task001.Task;
+
 /**111
  * Created by User on 25.03.2016.
  */
-public class MyClass {
+public class MyClass implements Task{
+
+
+    public boolean isPositive(int number) {
+        if (number>=0) {
+            return true;
+        }
+        return false;
+    }
 }
