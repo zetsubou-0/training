484be2944cdc5e92f07da21337ea2d9beacdb80e
Torus <valiktorus@mail.ru>
2016-09-08 01:11:00 +0200

    Collections

---
+++ trainees/valentin_pshenichnik/src/main/java/tasks/task007/Collection.java
@@ -0,0 +1,52 @@
+package tasks.task007;
+
+import com.zetsubou_0.training.task.task007.Task;
+import com.zetsubou_0.training.task.task007.been.Animal;
+import com.zetsubou_0.training.task.task007.been.AnimalType;
+
+import java.util.*;
+
+/**
+ * Created by Torus on 08.09.2016.
+ */
+public class Collection implements Task {
+    @Override
+    public List<Animal> reverse(List<Animal> animals) {
+        List<Animal> newAnimal = new ArrayList<>();
+        ListIterator<Animal> listIterator = animals.listIterator();
+        while (listIterator.hasPrevious()){
+            newAnimal.add(listIterator.previous());
+        }
+        return newAnimal;
+    }
+
+    @Override
+    public List<Animal> getAnimalsByType(List<Animal> animals, AnimalType type) {
+
+        List<Animal> newList = new ArrayList<>();
+        for (Animal animal:animals) {
+            if (animal.getType().equals(1)){
+                newList.add(animal);
+            }
+        }
+        return newList;
+    }
+
+    @Override
+    public List<Animal> getUniqueList(List<Animal> animals) {
+        Set<Animal> set = new HashSet<>();
+        List<Animal> newList = new ArrayList<>();
+        for (Animal animal: animals) {
+            set.add(animal);
+        }
+        for (Animal animal: set) {
+            newList.add(animal);
+        }
+        return newList;
+    }
+
+    @Override
+    public Map<AnimalType, SortedSet<Animal>> sortAnimals(List<Animal> animals) {
+        return null;
+    }
+}
--- trainees/valentin_pshenichnik/src/test/java/com/zetsubou_0/training/test/trainees/task007/Test.java
+++ trainees/valentin_pshenichnik/src/test/java/com/zetsubou_0/training/test/trainees/task007/Test.java
@@ -1,11 +1,12 @@
 package com.zetsubou_0.training.test.trainees.task007;
 
 import com.zetsubou_0.training.test.task007.TaskTest;
+import tasks.task007.Collection;
 
 public class Test extends TaskTest {
 
     @Override
     public void init() throws IllegalAccessException, InstantiationException {
-//        setupClass(MyClass.class);
+        setupClass(Collection.class);
     }
 }
